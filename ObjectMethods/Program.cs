395a28f6d80387d7405579b4de627a1066963f60
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-02-04 16:51:08 -0600

    Add project files.

---
+++ ObjectMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ObjectMethods // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Jim", "Business", 2.8);
+            Student student2 = new Student("Pam", "Art", 3.6);
+
+            Console.WriteLine(student1.HasHonors()); //Return False because Jim is not that smart.
+            Console.WriteLine(student2.HasHonors()); //Return true because Pam is smart.
+
+            Console.ReadLine();
+        }
+    }
+}
+
+/*
+ Topic: Object Methods
+What are object methods?
+Methods are used to perform certain actions, and they are also known as functions.
+
+Why use methods? To reuse code: define the code once, and use it many times.
+
+So let's say that we're trying to find out which students are in the honor roll.
+ */
---
+++ ObjectMethods/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectMethods
+{
+    internal class Student
+    {
+            public string name;
+            public string major;
+            public double gpa;
+        
+
+       public Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+        //create a method whether or not the students have honors.
+        //when jim calls this method it will use his gpa vice versa Pam.
+        //the methods are powerful because they can get different information depending on the value of their attributes.
+        //you can make as many methods you want from line 27 - 34
+        public bool HasHonors()
+         {
+            if(gpa >= 2.5) //you can change the gpa and now the students will be honors just by changing line 28. this all you to define different rules.
+                {
+                    return true;
+                }
+            return false;
+         }
+
+
+    }
+
+    
+}
+
+
+
+//I defined three attributes. a student will have a name, major, and gpa.
